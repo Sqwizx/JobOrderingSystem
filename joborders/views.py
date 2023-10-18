dbf9b063a9d7cc1ffa9889c3f4e0159d3fde2e41
Sqwizx <azzamhamiludin@gmail.com>
2023-10-18 11:23:09 +0800

    added a calendar to pick dates
    views.py for joborders
    recipe.js changes

--- joborders/views.py
+++ joborders/views.py
@@ -6,7 +6,14 @@ from django.contrib.auth.decorators import login_required
 
 @login_required
 def create_joborder(request):
-      current_date = datetime.now()
-      days = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']  # Adjust the order based on your requirements
-      date_info = [{'date': (current_date + timedelta(days=i)).strftime('%d'), 'day': days[(current_date + timedelta(days=i)).weekday()]} for i in range(7)]
-      return render(request, 'create_joborder.html', {'date_info': date_info, 'current_date': current_date})
+    if request.method == 'GET' and 'selected_date' in request.GET:
+        selected_date = request.GET.get('selected_date')
+        current_date = datetime.strptime(selected_date, '%Y-%m-%d')
+        days = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+        date_info = [{'date': (current_date + timedelta(days=i)).strftime('%d'), 'day': days[(current_date + timedelta(days=i)).weekday()]} for i in range(7)]
+        return JsonResponse({'date_info': date_info, 'current_date': current_date.strftime('%Y-%m-%d')})
+    else:
+        current_date = datetime.now()
+        days = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+        date_info = [{'date': (current_date + timedelta(days=i)).strftime('%d'), 'day': days[(current_date + timedelta(days=i)).weekday()]} for i in range(7)]
+        return render(request, 'create_joborder.html', {'date_info': date_info, 'current_date': current_date})
