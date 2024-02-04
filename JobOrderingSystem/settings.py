6f748fbaf304a02ab8da4f1ba0a9213d04180a78
Sqwizx <azzamhamiludin@gmail.com>
2024-02-04 20:32:35 +0800

    postgres

--- JobOrderingSystem/settings.py
+++ JobOrderingSystem/settings.py
@@ -85,11 +85,12 @@ WSGI_APPLICATION = 'JobOrderingSystem.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-        'OPTIONS': {
-            'timeout': 30,  # Set timeout to 30 seconds
-        },
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT'),
     }
 }
 
